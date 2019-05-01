24539adccfbc0839ba78d8673ac3899b6425b0af
David Adell <dadell@seidor.es>
2019-05-01 18:32:07 +0200

    Create project with initial Calculator UI

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by David Adell on 1/5/19.
+//  Copyright © 2019 djadell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
